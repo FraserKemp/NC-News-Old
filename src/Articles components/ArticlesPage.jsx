4e894a6bcdc06d442d212cd6d4626ab2e47a54cf
Fraser <fraser@Roberts-MacBook-Pro.local>
2019-06-03 16:27:14 +0100

    created a request to get all the articles

--- src/Articles components/ArticlesPage.jsx
+++ src/Articles components/ArticlesPage.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import Title from '../Title components/Title';
+import axios from 'axios';
 
 export default class ArticlesPage extends Component {
+  state = {
+    articles: []
+  };
+
+  componentDidMount() {
+    const url = 'https://fk-news-app.herokuapp.com/api/articles';
+    axios.get(url).then(({ data: { articles } }) => {
+      this.setState({ articles });
+    });
+  }
+
   render() {
     return (
       <div>
